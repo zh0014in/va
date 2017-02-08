19d2b6a4b34f5a0ed70b5d5e6f5e641e5ac205ce
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-02-08 20:57:55 +0800

    testing MutationObserver

--- owncloud/apps/files/js/virtualAssistant.js
+++ owncloud/apps/files/js/virtualAssistant.js
@@ -1,11 +1,31 @@
 $(document).ready(function () {
 
-$("#va-upload-form").bind('ajax:complete', function() {
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        eventListenerSupported = window.addEventListener;
 
-         // tasks to do 
-alert('uploaded');
+    return function(obj, callback){
+        if( MutationObserver ){
+            // define a new observer
+            var obs = new MutationObserver(function(mutations, observer){
+                if( mutations[0].addedNodes.length )// only when node is added
+                    callback();
+            });
+            // have the observer observe foo for changes in children
+            obs.observe( obj, { childList:true, subtree:true });
+        }
+        else if( eventListenerSupported ){
+            
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            obj.addEventListener('DOMNodeRemoved', callback, false);
+        }
+    }
+})();
 
-   });
+// Observe a specific DOM element:
+observeDOM( document.getElementById('fileList') ,function(){ 
+    console.log('file list changed');
+});
 
     $("#virtualAssistant").dialog({
         resizable: false,
