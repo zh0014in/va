588052f817779140d2bab46e2549e0f408fa3b81
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-02-11 15:09:32 +0800

    detect file list change

---
+++ owncloud/apps/files_comments/schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','types','filesystem')
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','enabled','false')
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','installed_version','0.0.1')
