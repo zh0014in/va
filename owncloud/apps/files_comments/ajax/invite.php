f7e886c2eaaf237fe3fe7c394f57845e86961131
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-02-12 21:39:51 +0800

    comment

--- owncloud/apps/files_comments/ajax/invite.php
+++ owncloud/apps/files_comments/ajax/invite.php
@@ -4,36 +4,31 @@ require_once(OC::$APPSROOT . '/apps/files_comments/lib_comment.php');
 OCP\JSON::checkAppEnabled('files_comments');
 OCP\JSON::checkLoggedIn();
 
-$userDirectory = '/'.OCP\USER::getUser().'/files';
+$userDirectory = '/' . OCP\USER::getUser() . '/files';
 $sources = explode(';', $_POST['sources']);
 $uid_commenting_with = $_POST['uid_commenting_with'];
 $permissions = $_POST['permissions'];
 foreach ($sources as $filepath) {
-	$file = OC_FileCache::get($filepath);
-	$path = ltrim($filepath, '/');
-	$filepath = $userDirectory.$filepath;
-	// Check if the file exists or if the file is being reshared
-	if ($filepath && $file['encrypted'] == false && (OC_FILESYSTEM::file_exists($path) && OC_FILESYSTEM::is_readable($path) || OC_Comment::getFilePath($filepath))) {
-		try {
-			$invited = new OC_Comment($filepath, $uid_commenting_with, $permissions);
-			// If this is a private link, return the token
-			if ($uid_commenting_with == OC_Comment::PUBLICLINK) {
-				OCP\JSON::success(array('data' => $invited->getToken()));
-			} else {
-				OCP\JSON::success();
-			}
-		} catch (Exception $exception) {
-			OCP\Util::writeLog('files_comments', 'Unexpected Error : '.$exception->getMessage(), OCP\Util::ERROR);
-			OCP\JSON::error(array('data' => array('message' => $exception->getMessage())));
-		}
-	} else {
-		if ($file['encrypted'] == true) {
-			OCP\JSON::error(array('data' => array('message' => 'Encrypted files cannot be shared')));
-		} else {
-			OCP\Util::writeLog('files_comments', 'File does not exist or is not readable :'.$filepath, OCP\Util::ERROR);
-			OCP\JSON::error(array('data' => array('message' => 'File does not exist or is not readable')));
-		}
-	}
+    $file = OC_FileCache::get($filepath);
+    $path = ltrim($filepath, '/');
+    $filepath = $userDirectory . $filepath;
+    // Check if the file exists or if the file is being reshared
+    if ($filepath && $file['encrypted'] == false && (OC_FILESYSTEM::file_exists($path) && OC_FILESYSTEM::is_readable($path) || OC_Comment::getFilePath($filepath))) {
+        try {
+            $invited = new OC_Comment($filepath, $uid_commenting_with, $permissions);
+            OCP\JSON::success();
+        } catch (Exception $exception) {
+            OCP\Util::writeLog('files_comments', 'Unexpected Error : ' . $exception->getMessage(), OCP\Util::ERROR);
+            OCP\JSON::error(array('data' => array('message' => $exception->getMessage())));
+        }
+    } else {
+        if ($file['encrypted'] == true) {
+            OCP\JSON::error(array('data' => array('message' => 'Encrypted files cannot be shared')));
+        } else {
+            OCP\Util::writeLog('files_comments', 'File does not exist or is not readable :' . $filepath, OCP\Util::ERROR);
+            OCP\JSON::error(array('data' => array('message' => 'File does not exist or is not readable')));
+        }
+    }
 }
 
 ?>
