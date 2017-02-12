d3316a7c31006ec3d0a74b1bbcf451b3788481f3
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-02-12 16:34:09 +0800

    comment

--- owncloud/localhost.sql
+++ owncloud/localhost.sql
@@ -560,3 +560,18 @@ CREATE TABLE IF NOT EXISTS `oc_users` (
 
 INSERT INTO `oc_users` (`uid`, `password`) VALUES
 ('student', '$2a$08$b7tfS9Zwao4rbC1YrSZ.uOrOi4NzKLsnwyWNdZpZJ9aMKlLS46tfy');
+
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','types','filesystem')
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','enabled','false')
+INSERT INTO `oc_appconfig`(`appid`, `configkey`, `configvalue`) VALUES ('files_comments','installed_version','0.0.1')
+CREATE TABLE IF NOT EXISTS `oc_commenting` (
+  `uid_owner` varchar(64) NOT NULL DEFAULT '',
+  `uid_commenting_with` varchar(64) NOT NULL DEFAULT '',
+  `filepath` varchar(128) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `oc_comments` (
+  `uid_owner` varchar(64) NOT NULL DEFAULT '',
+  `uid_createdby` varchar(64) NOT NULL DEFAULT '',
+  `filepath` varchar(128) NOT NULL DEFAULT '',
+  `body` varchar(128) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
